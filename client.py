a46d5b8a3b9b67c6b42209c11406faf229e3c397
Zhou <134062168+srzhouz@users.noreply.github.com>
2024-04-29 12:13:30 +0200

    Prueba

---
+++ client.py
@@ -0,0 +1,36 @@
+# client.py
+
+#importing socket module
+import socket
+
+# creating socket instance
+client_object = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+
+# target ip address and port
+ip_address = '192.168.1.175'
+port = 5555
+
+# instance requesting for connection to the specified address and port
+client_object.connect((ip_address,port))
+
+# receiving response from server
+data_receive = client_object.recv(1024)
+
+# if response is not null
+if data_receive:
+	# Connection is successful
+    print("CLIENT CONNECTED TO SERVER")
+    print(data_receive.decode('utf-8'))
+    
+    
+    while data_receive:
+    	# user input
+        client_input = input().encode('utf-8')
+        
+        # sending request to the server
+        client_object.send(client_input)
+        
+        # receiving response from the server
+        data_receive = client_object.recv(1024)
+        if data_receive:
+            print("{}: {}".format("SERVER",data_receive.decode('utf-8')))
---
+++ server.py
@@ -0,0 +1,34 @@
+# server.py
+# Importing neccessary inbuilt modules
+import socket
+import random
+import string
+
+# Creating a socket instance
+server_object = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+
+# Connecting to the localhost
+ip_address = '192.168.1.114'
+port = 5555
+server_object.bind((ip_address, port))
+server_object.listen()
+
+#Once the client connects to the particular port, the server starts to accept the request.
+connection_object, _ = server_object.accept()
+
+
+if connection_object:
+	# Connected to client successfully
+    print("SERVER CONNECTED TO CLIENT")
+    
+    # sending initial message to the client
+    connection_object.send(b"type the message")
+    
+    # receiving message from the client
+    data_receive = connection_object.recv(1024)
+    
+    while data_receive != b'stop':
+        print("{}: {}".format("CLIENT MESSAGE: ", data_receive.decode('utf-8')))
+        server_input = random.choice(string.ascii_letters)
+        connection_object.send(server_input.encode('utf-8'))
+        data_receive = connection_object.recv(1024)
